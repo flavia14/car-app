ac625762b0585919bd37ec4051c96a551b318535
OEM Configuration (temporary user) <flavia.andron@pitech.webhelp.com>
2023-03-28 13:20:47 +0300

    Add newline at the end of file.

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+
 class CommentController extends AbstractController
 {
     public function getListOfComment(): Response
--- src/Controller/UserProfileController.php
+++ src/Controller/UserProfileController.php
@@ -2,33 +2,16 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Entity\UserProfile;
 use App\Repository\UserProfileRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class UserProfileController extends AbstractController
 {
     #[Route('/profile-list', name: 'profile-list')]
     public function getListOfProfile(UserProfileRepository $profileRepository): Response
     {
-
-//        $user = new User();
-//        $user->setUsername('Vasile')
-//            ->setPassword('1234');
-//
-//        $userProfile = new UserProfile();
-//        $userProfile->setUser($user);
-//
-//        $profileRepository->save($userProfile,true);
-//
-//        return $this->render('user/index.html.twig', [
-//            'userProfile' => $userProfile
-//        ]);
-
-
         return new Response("this is the profile list");
     }
 }
