199f410205af7b5ffb026586a00c22264983fc26
OEM Configuration (temporary user) <flavia.andron@pitech.webhelp.com>
2023-03-28 09:09:11 +0300

    set-up and create User entity.

---
+++ src/Controller/UserController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user', name: 'user')]
+    public function index(UserRepository $user): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'user' => $user->findAll()
+        ]);
+    }
+
+    #[Route('/user/{user}', name: 'showOne')]
+    public function showOne(User $user): Response
+    {
+        return $this->render('user/showOne.html.twig', [
+            'user' => $user
+        ]);
+    }
+
+    #[Route('/user/add', name: 'user_add', priority: 2)]
+    public function add(Request $request, UserRepository $userRepository): Response
+    {
+        $form = $this->createForm(UserType::class, new User());
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $userRepository->save($user, true);
+            $this->addFlash('success', 'New user have been added');
+
+            return $this->redirectToRoute('user');
+        }
+        return $this->renderForm(
+            'user/createUser.html.twig',
+            [
+                'form' => $form
+            ]
+        );
+    }
+
+    #[Route('/user/update/{id}', name: 'user-update')]
+    public function update(User $user, Request $request, UserRepository $userRepository): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $userRepository->save($user, true);
+            $this->addFlash('success', 'New user have been update');
+
+            return $this->redirectToRoute('user');
+        }
+        return $this->renderForm(
+            'user/createUser.html.twig',
+            [
+                'form' => $form
+            ]
+        );
+    }
+}
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $firstUser = new User();
+        $firstUser->setUsername('username')
+            ->setPassword('pass');
+
+        $manager->persist($firstUser);
+
+        $manager->flush();
+    }
+}
