4d13fc6e5956a865009114f1f8cc7262ab4e4ad4
OEM Configuration (temporary user) <flavia.andron@pitech.webhelp.com>
2023-04-07 14:48:11 +0300

    Login functionality - PMLA: solving comments.

--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -4,18 +4,25 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use App\Enum\ExceptionCodes;
+use App\Exception\BadCredentialsException;
+use http\Env\Response;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserService
 {
+    /**
+     * @throws BadCredentialsException
+     */
     public function getLastUsername(AuthenticationUtils $utils): array
     {
-        $lastUsername = $utils->getLastUsername();
         $error = $utils->getLastAuthenticationError();
+        if ($error){
+            throw new BadCredentialsException($error->getMessage(), 400);
+        }
 
-        return [
-            'lastUsername' => $lastUsername,
-            'error' => $error,
-        ];
+        $lastUsername = $utils->getLastUsername();
+
+        return ['lastUsername' => $lastUsername];
     }
 }
