a1cf84ee0472db3c56931320ef5161eb7ab429da
OEM Configuration (temporary user) <flavia.andron@pitech.webhelp.com>
2023-04-11 10:53:40 +0300

    Comment update PMLA.

--- src/Dto/CommentDto.php
+++ src/Dto/CommentDto.php
@@ -4,13 +4,7 @@ declare(strict_types=1);
 
 namespace App\Dto;
 
-use Symfony\Component\Validator\Constraints as Assert;
-
 class CommentDto
 {
-    /**
-     * @Assert\Type("string")
-     * @Assert\NotBlank()
-     */
     public string $text;
 }
--- src/Dto/Request/CommentRequestDto.php
+++ src/Dto/Request/CommentRequestDto.php
@@ -2,7 +2,13 @@
 
 namespace App\Dto\Request;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class CommentRequestDto
 {
+    /**
+     * @Assert\Type("string")
+     * @Assert\NotBlank()
+     */
     public string $text;
 }
