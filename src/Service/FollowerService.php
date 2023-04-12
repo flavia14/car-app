06ec42828ce9817fbca3dbd75959ac538263d10e
OEM Configuration (temporary user) <flavia.andron@pitech.webhelp.com>
2023-04-12 12:02:33 +0300

    Solving comments - followerService.

--- src/Service/FollowerService.php
+++ src/Service/FollowerService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Entity\User;
+use App\Exception\CanNotFollowCurrentUserException;
 use Doctrine\Persistence\ManagerRegistry;
 
 class FollowerService
@@ -16,19 +17,30 @@ class FollowerService
         $this->managerRegistry = $managerRegistry;
     }
 
+    /**
+     * @throws CanNotFollowCurrentUserException
+     */
     public function follow(User $currentUser, User $userToFollow): void
     {
-        if ($userToFollow->getId() !== $currentUser->getId()) {
-            $currentUser->addFollow($userToFollow);
-            $this->managerRegistry->getManager()->flush();
+        if ($userToFollow->getId() === $currentUser->getId()) {
+            throw new CanNotFollowCurrentUserException("You can not follow your account.");
         }
+
+        $currentUser->addFollow($userToFollow);
+        $this->managerRegistry->getManager()->flush();
     }
 
+    /**
+     * @throws CanNotFollowCurrentUserException
+     */
     public function unfollow(User $currentUser, User $userToUnFollow): void
     {
-        if ($userToUnFollow->getId() !== $currentUser->getId()) {
-            $currentUser->removeFollow($userToUnFollow);
-            $this->managerRegistry->getManager()->flush();
+
+        if ($userToUnFollow->getId() === $currentUser->getId()) {
+            throw new CanNotFollowCurrentUserException("You can not unfollow your account.");
         }
+
+        $currentUser->removeFollow($userToUnFollow);
+        $this->managerRegistry->getManager()->flush();
     }
 }
