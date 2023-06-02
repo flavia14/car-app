1fca767d708b021ad4ac7e9aae129f57bcbfc08b
Flavia Andron <flavia.andron@pitech.webhelp.com>
2023-06-02 18:45:19 +0300

    Finish the table.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -62,17 +62,7 @@ class RegistrationController extends BaseController
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
-        // validate email confirmation link, sets User::isVerified=true and persists
-        try {
-            $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
-        } catch (VerifyEmailExceptionInterface $exception) {
-            $this->addFlash('verify_email_error', $exception->getReason());
-
-            return $this->redirectToRoute('app_register');
-        }
-
-        // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
 
         return $this->redirectToRoute('app_register');
     }
